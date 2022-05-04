b8a19f1de5d14d2f9f6f3b732352a97b27137bc1
richirrim <ricardoortega.dev@gmail.com>
2022-05-04 12:51:52 -0500

    Refactor en el nombre del method.

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -50,19 +50,19 @@ describe('StudentService class', () => {
 
   describe('Static method: getStudentWithCreditsGreaterThan', () => {
     it('Devuelve todos los estudiantes con creditos mayores a 500', () => {
-      const studentsList = StudentService.getStudentWithCreditsGreaterThan(students)
+      const studentsList = StudentService.getStudentWithCreditsGreaterThan500(students)
 
       expect(studentsList[0].credits).toBeGreaterThan(500)
       expect(studentsList[1].credits).toBeGreaterThan(500)
     })
 
     it('Validad que el array devuelto no este empty', () => {
-      const studentsList = StudentService.getStudentWithCreditsGreaterThan(students)
+      const studentsList = StudentService.getStudentWithCreditsGreaterThan500(students)
       expect(studentsList).not.toHaveLength(0)
     })
 
     it('Validad que el elemento devuelto sea un array', () => {
-      const studentsList = StudentService.getStudentWithCreditsGreaterThan(students)
+      const studentsList = StudentService.getStudentWithCreditsGreaterThan500(students)
       expect(Array.isArray(studentsList)).toBeTruthy()
     })
   })
